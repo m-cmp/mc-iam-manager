4d7a4f2ccceb559590bb88177bbe2416dc9544e1
jazmandorf <jazmandorf@gmail.com>
2023-11-07 09:23:20 +0900

    modify something

--- grifts/init.go
+++ grifts/init.go
@@ -8,4 +8,12 @@ import (
 
 func init() {
 	buffalo.Grifts(actions.App())
+	//나중에 default bind setting 을 위해 처리
+	// binding.Register("application/json", func(r *http.Request, i interface{}) error {
+	// 	b, err := io.ReadAll(r.Body)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	return json.Unmarshal(b, i)
+	// })
 }
