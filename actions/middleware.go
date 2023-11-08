f893101455ac796fde6cd72210c7017e18fa952a
raccoon-pi <wsx1341@gmail.com>
2023-11-08 00:06:50 +0000

    middleware status

--- actions/middleware.go
+++ actions/middleware.go
@@ -1,7 +1,6 @@
 package actions
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
@@ -13,7 +12,7 @@ func IsAuth(next buffalo.Handler) buffalo.Handler {
 		AccessToken := c.Request().Header.Get("Authorization")
 
 		// 아래로 DecodeAccessToken 을 사용
-		token, tokenMap, err := KC_client.DecodeAccessToken(c, AccessToken, KC_realm)
+		token, _, err := KC_client.DecodeAccessToken(c, AccessToken, KC_realm)
 		// token 이 expire 되면 err 로그 출력 됨 : could not decode accessToken with custom claims: Token is expired
 		if err != nil {
 			return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
@@ -27,14 +26,14 @@ func IsAuth(next buffalo.Handler) buffalo.Handler {
 			}))
 		}
 
-		fmt.Println("******************************")
-		fmt.Println("DecodeAccessToken err =", err)
-		fmt.Println("******************************")
-		fmt.Println("DecodeAccessToken token =", token)
-		fmt.Println("******************************")
-		fmt.Println("DecodeAccessToken tokenMapClaims =", tokenMap)
-		fmt.Println("******************************")
-		fmt.Println("DecodeAccessToken token.Valid =", token.Valid)
+		// fmt.Println("******************************")
+		// fmt.Println("DecodeAccessToken err =", err)
+		// fmt.Println("******************************")
+		// fmt.Println("DecodeAccessToken token =", token)
+		// fmt.Println("******************************")
+		// fmt.Println("DecodeAccessToken tokenMapClaims =", tokenMap)
+		// fmt.Println("******************************")
+		// fmt.Println("DecodeAccessToken token.Valid =", token.Valid)
 
 		return next(c)
 	}
