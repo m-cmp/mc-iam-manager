722af2be18aa8487d8d5af53c7731414915798dc
raccoon-pi <wsx1341@gmail.com>
2024-08-14 05:57:39 +0000

    add workspace user role mapping with keycloak

--- actions/users.go
+++ actions/users.go
@@ -9,21 +9,16 @@ import (
 	"github.com/m-cmp/mc-iam-manager/handler/keycloak"
 )
 
-type createUserRequset struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
-}
-
 func CreateUser(c buffalo.Context) error {
 	accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
 
-	createUserReq := &createUserRequset{}
+	createUserReq := &keycloak.CreateUserRequset{}
 	if err := c.Bind(createUserReq); err != nil {
 		log.Println(err)
-		return c.Render(http.StatusBadRequest, r.JSON(err))
+		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"errors": err.Error()}))
 	}
 
-	err := keycloak.KeycloakCreateUser(accessToken, createUserReq.Name, createUserReq.Password)
+	err := keycloak.KeycloakCreateUser(accessToken, *createUserReq, createUserReq.Password)
 	if err != nil {
 		log.Println(err)
 		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"errors": err.Error()}))
@@ -52,6 +47,26 @@ func DeleteUser(c buffalo.Context) error {
 	userId := c.Param("userId")
 
 	err := keycloak.KeycloakDeleteUser(accessToken, userId)
+	if err != nil {
+		log.Println(err)
+		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"errors": err.Error()}))
+	}
+
+	return c.Render(http.StatusOK, r.JSON(map[string]string{"status": "success"}))
+}
+
+func UpdateUser(c buffalo.Context) error {
+	accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+
+	userId := c.Param("userId")
+
+	createUserReq := &keycloak.CreateUserRequset{}
+	if err := c.Bind(createUserReq); err != nil {
+		log.Println(err)
+		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"errors": err.Error()}))
+	}
+
+	err := keycloak.KeycloakUpdateUser(accessToken, *createUserReq, userId)
 	if err != nil {
 		log.Println(err)
 		return c.Render(http.StatusBadRequest, r.JSON(map[string]string{"errors": err.Error()}))
