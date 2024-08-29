4de203843d8ae7fbb6d3f89a257d69c373d39e7e
raccoon-pi <wsx1341@gmail.com>
2024-08-29 02:22:04 +0000

    test proxy debugconsole

--- debugconsole/js/index.js
+++ debugconsole/js/index.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 
 // GET 요청 예제
 axios.get('/proxy/ifconfig.me/get/')
-  .then(response => {
-    console.log('GET Response:@@@@', response.data);
-  })
-  .catch(error => {
-    console.error('Error in GET request:', error);
-  });
+    .then(response => {
+        console.log('GET Response:@@@@', response.data);
+    })
+    .catch(error => {
+        console.error('Error in GET request:', error);
+    });
 
 // // POST 요청 예제
 // axios.post('http://ifconfig.me', )
@@ -17,4 +17,5 @@ axios.get('/proxy/ifconfig.me/get/')
 //   })
 //   .catch(error => {
 //     console.error('Error in POST request:', error);
+
 //   });
