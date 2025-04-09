a49d3aeb744117b8fdd5cc6c28c14c2731ad79b5
dogfootman <idogfootman@gmail.com>
2025-04-10 08:33:43 +0900

    pkg 폴더 제거

--- pkg/mod/github.com/jackc/pgx/v5@v5.7.4/testsetup/README.md
+++
@@ -1,3 +0,0 @@
-# Test Setup
-
-This directory contains miscellaneous files used to setup a test database.
--- pkg/mod/github.com/jackc/puddle/v2@v2.2.2/export_test.go
+++
@@ -1,9 +0,0 @@
-package puddle
-
-import "context"
-
-func (p *Pool[T]) AcquireRaw(ctx context.Context) (*Resource[T], error) {
-	return p.acquire(ctx)
-}
-
-var AcquireSemAll = acquireSemAll
