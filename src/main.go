1bdefd0a763cf3fec2dd642c8e16acd7aba5d096
dogfootman <80231499+dogfootman@users.noreply.github.com>
2026-04-03 13:08:57 +0900

    feat(M2-UG): add GET /api/users/me endpoint for self profile retrieval (#71)
    Co-authored-by: raccoon-mh <wsx1341@gmail.com>

--- src/main.go
+++ src/main.go
@@ -347,6 +347,7 @@ func main() {
 		users.DELETE("/id/:userId", userHandler.DeleteUser, middleware.PlatformRoleMiddleware(middleware.Manage))
 		users.POST("/id/:userId/status", userHandler.UpdateUserStatus, middleware.PlatformRoleMiddleware(middleware.Manage))
 		users.PUT("/id/:userId/password", userHandler.ResetUserPassword, middleware.PlatformRoleMiddleware(middleware.Manage))
+		users.GET("/me", userHandler.GetMyInfo)                  // 사용자 본인 정보 조회
 		users.PUT("/me/password", userHandler.ChangeMyPassword) // 사용자 본인 패스워드 변경
 
 		users.POST("/menus-tree/list", menuHandler.ListUserMenuTree)
